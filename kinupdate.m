1840b2c24bd8bb4e582202fba561ec2034431238
s130294 <s130294@student.dtu.dk>
2017-03-12 11:16:40 +0100

    Add files via upload
    time should be an input to kinupdate, as it only updates ONE sample. So kinupdate now only updates 1 sampletime

--- kinupdate.m
+++ kinupdate.m
@@ -1,17 +1,19 @@
-function newpose = kinupdate(pose, robotpar, ts, time, wheelspeed)
+function newpose = kinupdate(pose, robotpar, ts, wheelspeed)
 %This function takes a 
 %   pose: [x,y,theta] in INERTIAL FRAME (column vector)
-%   robotpar [wheel separation, radius right wheel, radius left wheel]
+%   robotpar [wheel separation, radius right wheel, radius left wheel] (row
+%   vector)
 %   sampletime [ts],
-%   time [time], 
-%   wheelspeed [angular velocity right wheel, angular velocity left wheel]
+%   wheelspeed [angular velocity right wheel, angular velocity left
+%   wheel](row vector)
 % The function returns
-%   newpose [x, y, theta] in the INERTIAL FRAME (row vector)
+%   newpose [x, y, theta] in the INERTIAL FRAME (column vector)
 %   ---
-%   ---
+%
 %   It is associated with 31388 Adv. Aut. Robots Course
 %   Gruop 8, spring 2017, DTU Electrical Engineering
 %   ---
+
 
 % Robot Parameters
 w = robotpar(1);
@@ -27,24 +29,19 @@ theta = pose(3);
 rightWheelSpeed = wheelspeed(1);
 leftWheelSpeed = wheelspeed(2);
 
-%A = [0.5, 0.5, 0; 0, 0. 1; 1/w, 0]
-
+% Constraints matrix
 b =[(rightWheelRadius*rightWheelSpeed)/2 + (leftWheelRadius*leftWheelSpeed)/2; ...
-    0; (rightWheelRadius*rightWheelSpeed)/(2*(w/2)) - (leftWheelRadius*leftWheelSpeed)/(2*(w/2))];
+    0; (rightWheelRadius*rightWheelSpeed)/(2*(w/2)) - (leftWheelRadius*leftWheelSpeed)/(2*(w/2))]
 
-R= [cos(theta), sin(theta), 0; -sin(theta), cos(theta), 0; 0, 0, 1];
+% Rotation matrix for converting between I and R
+R= [cos(theta), sin(theta), 0; -sin(theta), cos(theta), 0; 0, 0, 1]
 
+xi = R\b
 
-xi = R\b;
-%zeta = inv(R)*A*b'
+newpose = pose + xi*ts 
 
-% Pre-Allocation
-newPose = zeros(time/ts,3);
-Ts = ts;
-
-for i = 1 : time/ts
-    newPose(i,:) = pose + xi*ts;
-    ts = ts + Ts;
 end
 
-newpose = newPose;
+
+
+
